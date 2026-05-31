bc4468fadc28617706fce0f7b93c9b7fc6057137
gvelgekar-byte <gvelgekar@gmail.com>
2026-05-31 13:48:42 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2664,7 +2664,7 @@ var cnames_active = {
   "quizizz": "zaida04.github.io/Quizizz.js",
   "quizmaster": "mechtester.github.io/quizmaster",
   "quotobot": "team-gigabyte.github.io/quotobot",
-  "r": "r-js.github.io",
+  ""reelio": "cname.vercel-dns.com",": "r-js.github.io",
   "ractive": "ractivejs.github.io",
   "rad": "rapid-application-development-js.github.io/RAD.JS", // noCF? (don´t add this in a new PR)
   "radashi": "radashi-org.github.io",
